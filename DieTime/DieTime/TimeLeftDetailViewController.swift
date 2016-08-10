c206646f3c19fc0cb41ce77e86aeebdd5b4e594c
charles <charleshkang1@gmail.com>
2016-08-10 15:26:34 -0400

    finished storyboard layout

---
+++ DieTime/DieTime/TimeLeftDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TimeLeftDetailViewController.swift
+//  DieTime
+//
+//  Created by Charles Kang on 8/10/16.
+//  Copyright © 2016 Charles Kang. All rights reserved.
+//
+
+import UIKit
+
+class TimeLeftDetailViewController: UIViewController {
+
+}
---
+++ DieTime/DieTime/TimeLeftViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LifeViewController.swift
+//  DieTime
+//
+//  Created by Charles Kang on 8/10/16.
+//  Copyright © 2016 Charles Kang. All rights reserved.
+//
+
+import UIKit
+
+class TimeLeftViewController: UIViewController
+{
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+}
